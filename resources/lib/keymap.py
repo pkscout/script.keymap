a3616cb70438c5d0eb12ea068c4ecf2b1ff7d65f
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2026-03-22 09:21:31 -0400

    fixes from Kodi AI review

--- resources/lib/keymap.py
+++ resources/lib/keymap.py
@@ -27,7 +27,8 @@ from resources.lib.utils import tr, settings
 
 default = xbmcvfs.translatePath('special://xbmc/system/keymaps/keyboard.xml')
 userdata = xbmcvfs.translatePath('special://userdata/keymaps')
-gen_file = os.path.join(userdata, '%s.xml' % settings('keymap_editor_filename'))
+gen_file = os.path.join(userdata, '%s.xml' %
+                        settings('keymap_editor_filename'))
 
 KODIMONITOR = xbmc.Monitor()
 
@@ -68,7 +69,8 @@ def main():
     confirm_discard = False
     while not KODIMONITOR.abortRequested():
         if (settings('enable_multifile') == 'true'):
-            idx = Dialog().select(tr(30000), [tr(30003), tr(30005), tr(30004), tr(30021)])
+            idx = Dialog().select(
+                tr(30000), [tr(30003), tr(30005), tr(30004), tr(30021)])
         else:
             idx = Dialog().select(tr(30000), [tr(30003), tr(30005), tr(30004)])
         if idx == 0:
@@ -79,16 +81,22 @@ def main():
         elif idx == 1:
             # backup any user defined keymaps
             if (settings('enable_multifile') == 'false'):
-                for name in os.listdir(userdata):
-                    if name.endswith('.xml') and name != os.path.basename(gen_file):
-                        src = os.path.join(userdata, name)
-                        for i in range(100):
-                            dst = os.path.join(userdata, "%s.bak.%d" % (name, i))
-                            if os.path.exists(dst):
-                                continue
-                            xbmcvfs.rename(src, dst)
-                            # successfully renamed
-                            break
+                try:
+                    for name in os.listdir(userdata):
+                        if name.endswith('.xml') and name != os.path.basename(gen_file):
+                            src = os.path.join(userdata, name)
+                            for i in range(100):
+                                dst = os.path.join(
+                                    userdata, "%s.bak.%d" % (name, i))
+                                if os.path.exists(dst):
+                                    continue
+                                xbmcvfs.rename(src, dst)
+                                # successfully renamed
+                                break
+                except Exception:
+                    utils.rpc('GUI.ShowNotification', title=tr(30000),
+                              message=tr(30001), image='error')
+                    continue
             # save
             if os.path.exists(gen_file):
                 xbmcvfs.copy(gen_file, gen_file + ".old")
@@ -103,9 +111,13 @@ def main():
         elif idx == 3:
             # rename the file
             filename = Dialog().input(tr(30021), defaultt=settings('keymap_editor_filename'))
-            if not filename:
-                break
-            utils.rename_keymap(filename)
+            if not filename.strip():
+                utils.rpc('GUI.ShowNotification', title=tr(
+                    30000), message=tr(30001), image='error')
+                continue
+            if not utils.rename_keymap(filename):
+                utils.rpc('GUI.ShowNotification', title=tr(
+                    30000), message=tr(30001), image='error')
             xbmc.executebuiltin("action(reloadkeymaps)")
         elif idx == -1 and confirm_discard:
             if Dialog().yesno(tr(30000), tr(30006)) == 1:
